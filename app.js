05d0c9bb64f60bcc56b65a1262205c06c80a71b4
Brian McGehee <doc@tavian.com>
2018-09-17 15:38:34 -0700

    port

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Web server listening on: ${port}`);
